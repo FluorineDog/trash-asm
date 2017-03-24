39fd149a68d862d07e94d8ccdc2d5dc8e2fed07b
FluorineDog <576268911@qq.com>
2017-03-24 16:23:44 +0800

    ha?

--- test42.asm
+++ test42.asm
@@ -1,1 +1,32 @@
 test on fucking windows git hidden functional
+aslkdfnasnvdklvas[vsdjvsav
+sadasl;vnsdvmsamdvsdlvsad
+vmsalc
+;samv;msadvlsadvsadmv;samvas;cvmasmc
+;cvmasmcdslac
+mas;
+cvlmsalc;samcsadcmsac
+sadmcmlsamdc;samcsakvnksalngsuabqaEGNAS
+GBASD
+GNKASDLGQWjgkjn
+;asd;vmaSKV ASDVJAS;DVJSAD KVMAS
+VDKSAJF
+SADKFNSANFDGSANKDFGASNGKASNVCSDKACXN;VF
+;kl;Lvnvn
+
+ cvlfvnk 
+ asdvcmsl;vsavmsdklvas
+ cvsdcvmdsa;cvlsa
+ mvas
+ vds;v
+ ;asnvkdsfvkasd
+ vnalsknvknasdvnasdnlac msacdmkdsakmasmc]
+ asdfvsakldvas
+ vdss;a sadasldvmlsdf
+ sadfsadl fdsaflsadjfgsad;
+ jasgsadgwioepg
+ wAFGNMDvm
+
+ ;asdnv
+ ;avmasdljvlsadmnvlasv
+ Lvnvn;asndv;a
