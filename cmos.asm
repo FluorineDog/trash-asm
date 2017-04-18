7ed0c2f1354e14dbeed93c0f33679bc84d77b70f
FluorineDog <576268911@qq.com>
2017-04-19 03:19:29 +0800

    done with 3

---
+++ cmos.asm
@@ -0,0 +1,38 @@
+.386
+stack   segment stack
+        dw 200 dup(?)
+stack   ends 
+include basis.inc
+data    segment 
+guard   db 2,?
+buf     db ?,?
+outp    db ?,?,'$'
+data    ends
+
+code    segment use16
+                assume cs:code, ss:stack, ds:data
+start:
+cmos    proc
+        mov ax, data
+        mov ds, ax
+
+        scanfs_ guard
+        mov al, buf
+        and al, 0fh
+        
+        out 70h, al
+        in  al, 71h
+
+        mov bl, al
+        and bl, 0fh
+        add bl, '0'
+        mov outp+1, bl
+        shr al, 4
+        add al, '0'
+        mov outp, al
+        puts outp
+        mov ax, 4c00h
+        int 21h
+cmos    endp
+code    ends
+end     start
--- make.bat
+++ make.bat
@@ -2,3 +2,4 @@ REM ml main.asm inputer.asm calcavg.asm sort.asm print.asm
 REM ml lab3.asm new.asm
 ml inst.asm
 ml uninst.asm
+ml cmos.asm
